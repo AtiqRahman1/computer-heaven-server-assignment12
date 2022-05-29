3865f0fff12a5b35e720850ccc831d7d2972511e
AtiqRahman1 <atiq94.ar@gmail.com>
2022-05-29 10:00:41 +0600

    update cors

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+    cors({
+        origin: true,
+        optionsSuccessStatus: 200,
+        credentials: true,
+    })
+);
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ntt2r.mongodb.net/?retryWrites=true&w=majority`;
